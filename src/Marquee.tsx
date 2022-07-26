b4e97eaf61dce25af7e62fec0109e99b565e2b01
Andres Kury <akurylec@gmail.com>
2022-07-26 18:46:58 -0300

    bump

--- src/Marquee.tsx
+++ src/Marquee.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useState } from "react"
 import "./App.css"
 import styled, { keyframes } from "styled-components"
