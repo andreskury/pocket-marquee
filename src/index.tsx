b035061502829797f004f46b1094e47abc22507c
Andres Kury <akurylec@gmail.com>
2022-07-27 09:47:02 -0300

    fix: rev1

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import Marquee from "./Marquee"
+import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 )
 root.render(
     <React.StrictMode>
-        <Marquee />
+        <App />
     </React.StrictMode>
 )
 
